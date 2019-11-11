716e712f3b57ef978481e0c64d012b0cd98c9a6f
Arthur Pannier <arthur.pannier@epita.fr>
2019-11-11 16:51:30 +0100

    [Exercises Readline_exec] add commands executions

--- exercises/readline-exec/src/readline_exec.c
+++ exercises/readline-exec/src/readline_exec.c
@@ -11,18 +11,52 @@
 #include <string.h>
 #include <unistd.h>
 
+void  parse(char *line, char **argv)
+{
+    char *delimit = " \t\n";
+    char *save;
+    argv[0] = strtok_r(line, delimit, &save);
+    char *token;
+    int i = 0;
+    while((token = strtok_r(NULL, delimit, &save)) != NULL)
+    {
+        i++;
+        argv[i] = token;
+    }
+    argv[++i] = '\0';
+}
+
+
+
+void execcmd(char **cmd)
+{
+    pid_t pid;
+    int stat;
+
+    pid = fork();
+    if (pid < 0)
+        err(666, "Hudson We have a problem!");
+    if (pid == 0)
+    {
+        execvp(*cmd, cmd);
+        exit(0);
+    }
+    else
+        wait(&stat);
+
+}
+
 int main(void)
 {
     char *input = NULL;
-
+    char *argv[128];
     while (1)
     {
         input = get_next_line("21sh$ ");
-
         if (!input)
             break;
-
-        printf("%s\n", input);
+        parse(input, argv);
+        execcmd(argv);
         free(input);
     }
 
