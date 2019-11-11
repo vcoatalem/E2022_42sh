ff5d1566d625a032e46fb2b87894b1ffd809791f
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-11 16:51:35 +0100

    [EXECUTION][TESTS] working on execution piping and testing

--- src/execution/redirection.c
+++ src/execution/redirection.c
@@ -1,33 +1,20 @@
 #include "execution.h"
 
-#if 0
-enum REDIRECTION_TYPE
-{
-    REDIRECTION_NONE,
-    STDIN_FROM_ARG,   // < arg
-    STDOUT_TO_ARG,    // 1> arg
-    STDERR_TO_ARG     // 2> arg
-    STDOUT_TO_STDERR, // 1>&2
-    STDERR_TO_STDOUT, // 2>&1
-};
-
-struct redirection
-{
-    enum REDIRECTION_TYPE type;
-    char *arg;
-};
-#endif
-
 struct redirection *redirection_init(enum REDIRECTION_TYPE type, char *arg)
 {
     struct redirection *res = calloc(1, sizeof(struct redirection));
     res->type = type;
-    res->arg = arg;
+    if (arg)
+        res->arg = strdup(arg);
     return res;
 }
 
 void redirection_free(struct redirection *redirection)
 {
+    if (!redirection)
+        return;
+    if (redirection->arg)
+        free(redirection->arg);
     free(redirection);
 }
 
@@ -37,20 +24,25 @@ void redirection_print(struct redirection *redirection)
             redirection->type, redirection->arg);
 }
 
+struct redirection *redirection_dup(struct redirection *redirection)
+{
+    return redirection_init(redirection->type, redirection->arg);
+}
+
 static void redirect(int from, int to)
 {
     printf("redirect %d to %d\n", from, to);
     dup2(to, from);
-    close(to);
 }
 
-int redirection_execute(struct redirection *redirection)
+int redirection_execute(struct command *cmd, struct redirection *redirection)
 {
     if (redirection->type == STDIN_FROM_ARG)
     {
         int fd = open(redirection->arg, O_RDONLY);
         if (fd == -1)
             return RETURN_ERROR;
+        cmd->fd_in = fd;
         redirect(STDIN_FILENO, fd); 
     }
     else if (redirection->type == STDOUT_TO_ARG
@@ -60,17 +52,24 @@ int redirection_execute(struct redirection *redirection)
         if (fd == -1)
             return RETURN_ERROR;
         if (redirection->type == STDOUT_TO_ARG)
+        {
             redirect(STDOUT_FILENO, fd);
+        }
         else
+        {
             redirect(STDERR_FILENO, fd);
+            cmd->fd_err = fd;
+        }
     }
     else if (redirection->type == STDOUT_TO_STDERR)
     {
         redirect(STDOUT_FILENO, STDERR_FILENO);
+        cmd->fd_out = STDERR_FILENO;
     }
     else if (redirection->type == STDERR_TO_STDOUT)
     {
         redirect(STDERR_FILENO, STDOUT_FILENO);
+        cmd->fd_err = STDOUT_FILENO;
     }
     return RETURN_SUCCESS;
 }
--- src/execution/tests/redirect_basic.test.c
+++ src/execution/tests/redirect_basic.test.c
@@ -1,11 +1,64 @@
 #include "../execution.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    struct redirection *r1 = redirection_init(STDOUT_TO_ARG, "output");
-    redirection_execute(r1);
-    printf("this is some text\n");
-    redirection_free(r1);
+    int q = argc > 1 ? atoi(*(argv + 1)) : 0;
+    printf("TEST: #%d\n", q);
+
+    struct pipe *p = pipe_init();
+    
+    char *hello_world[] = { "echo", "hello world!", NULL };
+    char *cat_makefile[] = { "cat", "Makefile", NULL };
+    char *in_file[] = { "echo", "im in file", NULL };
+    char *cat[] = { "cat", NULL };
+    char *echo_a[] = { "echo", "A", NULL };
+    char *echo_b[] = { "echo", "B", NULL };
+
+    char **commands[] =
+    {
+        hello_world,
+        cat_makefile,
+        in_file,
+        cat,
+        echo_a,
+        echo_b
+    };
+
+    struct redirection *redirect_output = redirection_init(STDOUT_TO_ARG, "output");
+    struct redirection *redirect_err = redirection_init(STDERR_TO_ARG, "output");
+    struct redirection *redirect_input = redirection_init(STDIN_FROM_ARG, "input");
+    struct redirection *redirect_output_to_stderr = redirection_init(STDOUT_TO_STDERR, NULL);
+    struct redirection *redirect_stderr_to_output = redirection_init(STDERR_TO_STDOUT, NULL);
+    
+    if (q == 0 || q == 1)
+    {
+        struct command *command = command_init(commands[q], NULL);
+        pipe_add_command(p, command);
+    }
+    else if (q == 2)
+    {
+        struct command *command = command_init(commands[q], NULL);
+        command_add_redirection(command, redirection_dup(redirect_output));
+        pipe_add_command(p, command);
+    }
+    else if (q == 3)
+    {
+        struct command *command = command_init(commands[q], NULL);
+        command_add_redirection(command, redirection_dup(redirect_input));
+        pipe_add_command(p, command);
+    }
+    int return_value = pipe_execute(p);
+
+    pipe_free(p);
+
+    redirection_free(redirect_output);
+    redirection_free(redirect_err);
+    redirection_free(redirect_input);
+    redirection_free(redirect_output_to_stderr);
+    redirection_free(redirect_stderr_to_output);
+    printf("test return value: %d\n", return_value);
+    return return_value;
 }
