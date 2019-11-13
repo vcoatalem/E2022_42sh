648e1983f8c656be4784032f586c6313b89dd1c0
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-13 18:30:43 +0100

    [BUILD][LEXER] working on lexer build system

---
+++ src/lexer/tests/test_basic.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+#include "../lexer.h"
+
+static int token_are_equal(struct token *t1, struct token *t2)
+{
+    return (t1->type == t2->type && t1->value && t2->value);
+}
+
+static int token_array_are_equal(struct token_array *a1, struct token_array *a2)
+{
+   if (a1->size != a2->size)
+   {
+        return 0;
+   }
+   else
+   {
+        for (size_t i = 0; i < a1->size; i++)
+        {
+            if (!token_are_equal(*(a1->tok_array + i), *(a2->tok_array + i)))
+            {
+                return 0;
+            }
+        }
+   }
+   return 1;
+}
+
+int main(int argc, char **argv)
+{
+    int q = (argc == 1 ? 0 : atoi(*(argv + 1)));
+    
+    char *cmds[] =
+    {
+        "echo a || cat b",
+        "foo> bar",
+        "",
+        "echo foo||catbar",
+        "if a then b elsedsdfs c",
+        "a&&b|c|| d||& e"
+    };
+    struct token_array *exp = token_array_init();
+    fprintf(stdout, "%s\n", *(cmds + q));
+    if (q == 0)
+    {
+        token_array_add(exp, token_init(TOKEN_WORD, "echo"));
+        token_array_add(exp, token_init(TOKEN_WORD, "a"));
+        token_array_add(exp, token_init(TOKEN_DOUBLE_PIPE, "||"));
+        token_array_add(exp, token_init(TOKEN_WORD, "cat"));
+        token_array_add(exp, token_init(TOKEN_WORD, "b"));
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    else if (q == 1)
+    {
+        token_array_add(exp, token_init(TOKEN_WORD, "foo"));
+        token_array_add(exp, token_init(TOKEN_GREAT, ">"));
+        token_array_add(exp, token_init(TOKEN_WORD, "bar"));
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    else if (q == 2)
+    {
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    else if (q == 3)
+    {
+        token_array_add(exp, token_init(TOKEN_WORD, "echo"));
+        token_array_add(exp, token_init(TOKEN_WORD, "foo"));
+        token_array_add(exp, token_init(TOKEN_DOUBLE_PIPE, "||"));
+        token_array_add(exp, token_init(TOKEN_WORD, "catbar"));
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    else if (q == 4)
+    {
+        token_array_add(exp, token_init(TOKEN_IF, "if"));
+        token_array_add(exp, token_init(TOKEN_WORD, "a"));
+        token_array_add(exp, token_init(TOKEN_THEN, "then"));
+        token_array_add(exp, token_init(TOKEN_WORD, "b"));
+        token_array_add(exp, token_init(TOKEN_WORD, "elsedsdfs"));
+        token_array_add(exp, token_init(TOKEN_WORD, "c"));
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    else if (q == 5)
+    {
+        token_array_add(exp, token_init(TOKEN_WORD, "a"));
+        token_array_add(exp, token_init(TOKEN_DOUBLE_AMPERSAND, "&&"));
+        token_array_add(exp, token_init(TOKEN_WORD, "b"));
+        token_array_add(exp, token_init(TOKEN_PIPE, "|"));
+        token_array_add(exp, token_init(TOKEN_WORD, "c"));
+        token_array_add(exp, token_init(TOKEN_DOUBLE_PIPE, "||"));
+        token_array_add(exp, token_init(TOKEN_WORD, "d"));
+        token_array_add(exp, token_init(TOKEN_DOUBLE_PIPE, "||"));
+        token_array_add(exp, token_init(TOKEN_AMPERSAND, "&"));
+        token_array_add(exp, token_init(TOKEN_WORD, "e"));
+        token_array_add(exp, token_init(TOKEN_EOF, ""));
+    }
+    struct lexer *l = lexer_alloc(cmds[q]);
+    struct token_array *array = l->arr;
+    int eq = token_array_are_equal(array, exp);
+    if (!eq)
+    {
+        //dump both token arrays
+    }
+    token_array_free(array);
+    token_array_free(exp);
+    lexer_free(l);
+    return eq == 1 ? 0 : 1;
+}
