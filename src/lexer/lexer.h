d6cd8694361f72fff7e48c1fe35c13a82fd12abf
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-14 20:15:48 +0100

    [LEXER] added prototype for lexer

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -7,6 +7,26 @@
 #include <string.h>
 #include "token/token.h"
 
+enum lexer_state
+{
+    STATE_NONE,                   //regular
+    STATE_LEXING_QUOTES,          //in a '. asking for more input
+    STATE_LEXING_DOUBLE_QUOTES,   //in a ". asking for more input
+    STATE_UNFINISHED,             //string could be parsed to a valid expression if given more characters
+    STATE_FINISHED,               //lexer successfully lexed string and parser returned a valid expression
+    STATE_ERROR                   //lexer successfully lexed string but parser returned an error
+}
+
+struct lexer
+{
+    //
+    enum lexer_state state;
+    // data
+    char *str;
+    size_t iterator;
+};
+
+
 struct token_array
 {
     struct token **tok_array;
@@ -24,6 +44,18 @@ void token_array_free(struct token_array *arr);
 void token_array_add(struct token_array *arr, struct token *token);
 void token_array_print(struct token_array *arr, FILE *out);
 
+
+//TODO: turn token_array_create into lex
 struct token_array *token_array_create(char *str);
+struct token_array *lex(struct lexer *lexer);
+
+//TODO: lexer basic init
+struct lexer *lexer_init(void);
+
+//TODO: fill lexer with strings (concat)
+void lexer_add_string(struct lexer *lexer, char *str);
+
+//TODO: empty lexer
+void lexer_clear(struct lexer *lexer);
 
 #endif /* ! LEXER_H */
