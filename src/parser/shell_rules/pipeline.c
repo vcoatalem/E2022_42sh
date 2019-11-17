064c976dee5721178bca448fbfe27fbc9e8a917d
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-17 20:48:18 +0100

    [PARSER][FIX] fixed pipelines rules; parser now returns proper values for the few tests we have. will work on ast construction

--- src/parser/shell_rules/pipeline.c
+++ src/parser/shell_rules/pipeline.c
@@ -1,45 +1,34 @@
 #include "../parser.h"
 
-//pipe -> '|' pipeline
+//pipe -> '|' command pipe
+//pipe -> epsilon
 void sh_rule_pipe(struct rule_array *rules)
 {
     struct rule *rule = rule_build(RULE_PIPE,
             symbol_create(TOKEN_PIPE, 0),
-            symbol_create(0, RULE_PIPELINE),
-            //symbol_create(0, RULE_OPTIONNAL_LINE_BREAKS),
+            symbol_create(0, RULE_COMMAND),
+            symbol_create(0, RULE_PIPE),
+            NULL);
+    struct rule *rule_eps = rule_build(RULE_PIPE,
+            symbol_epsilon(),
             NULL);
     rule_array_add(rules, rule);
+    rule_array_add(rules, rule_eps);
 }
 
 //pipeline -> command pipe
 //pipeline -> '!' command pipe
-//pipeline -> command
-//pipeline -> '!' command
-//pipeline -> epsilon
 void sh_rule_pipeline(struct rule_array *rules)
 {
     struct rule *rule_a = rule_build(RULE_PIPELINE,
             symbol_create(0, RULE_COMMAND),
-            symbol_create(0, RULE_PIPELINE),
+            symbol_create(0, RULE_PIPE),
             NULL);
     struct rule *rule_b = rule_build(RULE_PIPELINE,
             symbol_create(TOKEN_EXCLAMATION_POINT, 0),
             symbol_create(0, RULE_COMMAND),
-            symbol_create(0, RULE_PIPELINE),
-            NULL);
-    struct rule *rule_c = rule_build(RULE_PIPELINE,
-            symbol_create(0, RULE_COMMAND),
-            NULL);
-    struct rule *rule_d = rule_build(RULE_PIPELINE,
-            symbol_create(TOKEN_EXCLAMATION_POINT, 0),
-            symbol_create(0, RULE_COMMAND),
-            NULL);
-    struct rule *rule_eps = rule_build(RULE_PIPELINE,
-            symbol_epsilon(),
+            symbol_create(0, RULE_PIPE),
             NULL);
     rule_array_add(rules, rule_a);
     rule_array_add(rules, rule_b);
-    rule_array_add(rules, rule_c);
-    rule_array_add(rules, rule_d);
-    rule_array_add(rules, rule_eps);
 }
