79e50e62525ac74712955db3b499c813b04842f4
Arthur Pannier <arthur.pannier@epita.fr>
2019-11-17 22:50:20 +0100

    [MAIN] handle case for print the ast

--- src/main/execute.c
+++ src/main/execute.c
@@ -27,7 +27,12 @@ int execute_stdin(struct execution_bundle *bundle)
     {
         //run lexer + parser
         struct token_array *arr = token_array_create(line);
-        ast_execute(tmp_parse(arr));
+        struct ast *ast = tmp_parse(arr);
+        if (bundle->options->ast_print_is_set == 1)
+        {
+            ast_dot_print(ast, "ast.dot");
+        }
+        ast_execute(ast);
         //token_array_print(arr, stdout);
         token_array_free(arr);
         free(line);
@@ -64,7 +69,12 @@ int execute_interactive(struct execution_bundle *bundle)
             lexer_add_string(lexer, input);
             token_arrays_fusion(arr, lex(lexer));
         }
-        ast_execute(tmp_parse(arr));
+        struct ast *ast = tmp_parse(arr);
+        if (bundle->options->ast_print_is_set == 1)
+        {
+            ast_dot_print(ast, "ast.dot");
+        }
+        ast_execute(ast);
         //token_array_print(arr, stdout);
         token_array_free(arr);
         free(input);
@@ -81,19 +91,31 @@ int execute_cmd(struct execution_bundle *bundle, char *cmd)
     //TODO: load lexer with cmd, run lexing + parsing
     //and return regardless of lexing state
     struct token_array *arr = token_array_create(cmd);
-    ast_execute(tmp_parse(arr));
+    struct ast *ast = tmp_parse(arr);
+    if (bundle->options->ast_print_is_set == 1)
+    {
+        ast_dot_print(ast, "ast.dot");
+    }
+    ast_execute(ast);
     //token_array_print(arr, stdout);
     token_array_free(arr);
     return BASH_RETURN_OK;
 }
 
-int execute_script(struct execution_bundle *bundle, char* script)
+int execute_script(struct execution_bundle *bundle, char* script, int create)
 {
     appendhistory(script);
     if (!bundle)
         return BASH_RETURN_ERROR;
-    FILE *fd = fopen(script, "r");
-    if (!fd)
+    //printf("mode = %s\n", mode);
+    FILE *fd;
+    fd = fopen(script, "r");
+    if (fd == NULL && create == 1)
+    {
+        fclose(fd);
+        fd = fopen(script, "w");
+    }
+    if (fd == NULL)
     {
         printf("Error no such file or directory %s\n", script);
         return BASH_RETURN_ERROR;
@@ -103,6 +125,7 @@ int execute_script(struct execution_bundle *bundle, char* script)
     size_t size;
     struct lexer *lexer = lexer_init();
     struct token_array *arr = token_array_init();
+
     while (getline(&line, &size, fd) != -1)
     {
         lexer_add_string(lexer, line);
@@ -110,9 +133,12 @@ int execute_script(struct execution_bundle *bundle, char* script)
 
         free(line);
     }
+    fclose(fd);
     //run lexer + parser
-    ast_execute(tmp_parse(arr));
     //token_array_print(arr, stdout);
+    if (arr->size)
+        ast_execute(tmp_parse(arr));
+
     token_array_free(arr);
     return BASH_RETURN_OK;
 }
