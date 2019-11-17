04a12a42f21a4c04ef743c15f5a3f3f442a1eb17
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-17 03:08:25 +0100

    [PARSER][REFACTO] establishing new parser implementation structures

---
+++ src/parser/symbol.c
@@ -0,0 +1,57 @@
+#include "parser.h"
+
+struct symbol *symbol_epsilon(void)
+{
+    struct symbol *res = calloc(1, sizeof(struct symbol));
+    res->type = SYMBOL_EPSILON;
+    return res;
+}
+
+struct symbol *symbol_end(void)
+{
+    struct symbol *res = calloc(1, sizeof(struct symbol));
+    res->type = SYMBOL_END;
+    return res;
+}
+
+struct symbol *symbol_create(enum token_type token_type, enum rule_id rule_id)
+{
+    struct symbol *res = calloc(1, sizeof(struct symbol));
+    if (res->rule_id == RULE_NONE)
+    {
+        //terminal symbol
+        res->type = SYMBOL_TOKEN;
+        res->token_type = token_type;
+    }
+    else
+    {
+        res->type = SYMBOL_RULE;
+        res->rule_id = rule_id;
+    }
+    return res;
+}
+
+struct symbol *symbol_dup(struct symbol *s)
+{
+    return symbol_create(s->token_type, s->rule_id);
+}
+
+void symbol_print(struct symbol *s)
+{
+    if (s->type == SYMBOL_EPSILON)
+    {
+        printf("epsilon");
+    }
+    else if (s->type == SYMBOL_END)
+    {
+        printf("$");
+    }
+    else if (s->type == SYMBOL_RULE)
+    {
+        printf("{rule #%d}", s->rule_id);
+    }
+    else
+    {
+        printf("{token %s}", token_to_string(s->token_type));
+    }
+}
---
+++ src/parser/symbol_array.c
@@ -0,0 +1,87 @@
+#include "parser.h"
+
+struct symbol_array *symbol_array_init(void)
+{
+    int initial_capacity = 4;
+    struct symbol_array *res = calloc(1, sizeof(struct symbol_array));
+    res->array = calloc(1, sizeof(void*) * initial_capacity);
+    res->capacity = initial_capacity;
+    res->size = 0;
+    return res;
+}
+
+void symbol_array_add(struct symbol_array *symbols, struct symbol *s)
+{
+    symbols->size = symbols->size + 1;
+    if (symbols->size == symbols->capacity)
+    {
+        symbols->capacity *= 2;
+        symbols->array = realloc(symbols->array,
+            sizeof(void*) * symbols->capacity);
+    }
+    symbols->array[symbols->size - 1] = s;
+}
+
+struct symbol_array *symbol_array_dup(struct symbol_array *symbols)
+{
+    struct symbol_array *arr = calloc(1, sizeof(struct symbol_array));
+    arr->capacity = symbols->capacity;
+    arr->size = symbols->size;
+    arr->array = calloc(sizeof(void*), arr->capacity);
+    for (size_t i = 0; i < symbols->size; i++)
+    {
+        arr->array[i] = symbol_dup(symbols->array[i]);
+    }
+    return arr;
+}
+
+#if 0
+//used to prevent same tokens to appear multiple times in first() and next()
+int symbol_array_contains(struct symbol_array *arr, struct symbol *s)
+{
+    for (size_t i = 0; i < arr->size; i++)
+    {
+        if (s->type == SYMBOL_TOKEN
+            && s->token_type == arr->array[i]->token_type)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+#endif
+
+//used in first() and next() to build sub rule firsts and nexts
+void symbol_array_merge(struct symbol_array *s1, struct symbol_array *s2)
+{
+    if (!s1 || !s2)
+        return;
+    for (size_t i = 0; i < s2->size; i++)
+    {
+//        if (!symbol_array_contains(s1, s2->array[i]))
+//        {
+            symbol_array_add(s1, symbol_dup(s2->array[i]));
+//        }
+    }
+}
+
+void symbol_array_free(struct symbol_array *s)
+{
+    for (size_t i = 0; i < s->size; i++)
+    {
+        free(s->array[i]);
+    }
+    free(s->array);
+    free(s);
+}
+
+void symbol_array_print(struct symbol_array *s)
+{
+    printf("[");
+    for (size_t i = 0; i < s->size; i++)
+    {
+        printf(" ");
+        symbol_print(s->array[i]);
+    }
+    printf(" ]");
+}
