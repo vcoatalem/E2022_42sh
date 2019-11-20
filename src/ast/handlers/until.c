de6d4e656903178f7e203fa29b809e07b7a6ee5e
J <kyu.shulk@gmail.com>
2019-11-20 18:22:29 +0100

    [AST][HANDLER] Add until handler

--- src/ast/handlers/until.c
+++ src/ast/handlers/until.c
@@ -1,4 +1,38 @@
-#include "ast.h"
+#include "../ast.h"
+#include "../../execution/execution.h"
+
+static struct ast *find_op_until_body(struct ast *ast)
+{
+    for (size_t i = 0; i < ast->nb_children; i++)
+    {
+        if (ast->forest[i]->content.op_type == OPERATOR_UNTIL_BODY)
+            return ast->forest[i];
+    }
+
+    return NULL;
+}
+
+static struct ast *find_op_do(struct ast *ast)
+{
+    for (size_t i = 0; i < ast->nb_children; i++)
+    {
+        if (ast->forest[i]->content.op_type == OPERATOR_DO)
+            return ast->forest[i];
+    }
+
+    return NULL;
+}
+
+static struct ast *find_op_done(struct ast *ast)
+{
+    for (size_t i = 0; i < ast->nb_children; i++)
+    {
+        if (ast->forest[i]->content.op_type == OPERATOR_DONE)
+            return ast->forest[i];
+    }
+
+    return NULL;
+}
 
 int ast_handle_until(struct ast *ast)
 {
@@ -7,11 +41,15 @@ int ast_handle_until(struct ast *ast)
 
     int try_execute = AST_ERROR;
 
-    while ((try_execute = ast_execute(ast->forest[0])) == AST_ERROR)
+    struct ast *ast_until_body = find_op_until_body(ast);
+    struct ast *ast_do = find_op_do(ast);
+
+    while (try_execute == AST_ERROR)
     {
-        for (size_t i = 1; i < ast->nb_children; i++)
-            ast_execute(ast->forest[i]);
+        try_execute = ast_execute(ast_until_body);
+        ast_execute(ast_do);
     }
 
-    return try_execute;
+    struct ast *ast_done = find_op_done(ast);
+    return ast_execute(ast_done);
 }
