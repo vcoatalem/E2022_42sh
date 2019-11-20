52c9328f4d325a060f787df4a5d9ef27510ac946
J <kyu.shulk@gmail.com>
2019-11-20 15:58:15 +0100

    [AST][HANDLER] Changing and or handlers, keep execution all children instead of stopping when false

--- src/ast/handlers/and_or.c
+++ src/ast/handlers/and_or.c
@@ -4,25 +4,28 @@ int ast_handle_and(struct ast *ast)
 {
     if (ast == NULL)
         return AST_ERROR;
+
+    int try_execute = AST_SUCCESS;
     for (size_t i = 0; i < ast->nb_children; i++)
     {
-        int try_execute = ast_execute(ast->forest[i]);
-        if (try_execute != AST_SUCCESS)
-            return try_execute;
+        if (ast_execute(ast->forest[i]) == AST_ERROR)
+            try_execute = AST_ERROR;
     }
-    return AST_SUCCESS;
+
+    return try_execute;
 }
 
 int ast_handle_or(struct ast *ast)
 {
     if (ast == NULL)
         return AST_ERROR;
+
     int try_execute = AST_ERROR;
     for (size_t i = 0; i < ast->nb_children; i++)
     {
-        try_execute = ast_execute(ast->forest[i]);
-        if (try_execute == AST_SUCCESS)
-            return try_execute;
+        if (ast_execute(ast->forest[i]) == AST_SUCCESS)
+            try_execute = AST_SUCCESS;
     }
+
     return try_execute;
 }
