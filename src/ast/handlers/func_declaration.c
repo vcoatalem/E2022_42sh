db125948cf27eba40928611ab0139eb0c894c29d
J <kyu.shulk@gmail.com>
2019-11-21 17:00:24 +0100

    [AST][HANDLER] Adding func_declaration handler

---
+++ src/ast/handlers/func_declaration.c
@@ -0,0 +1,43 @@
+#include "../ast.h"
+#include "../../main/42sh.h"
+#include "../../hashtable/hashtablefunc.h"
+
+static struct ast *find_op_function_name(struct ast *ast)
+{
+    for (size_t i = 0; i < ast->nb_children; i++)
+    {
+        if (ast->forest[i]->content.op_type == OPERATOR_FUNC_NAME)
+            return ast->forest[i];
+    }
+
+    return NULL;
+}
+
+static struct ast *find_op_function_body(struct ast *ast)
+{
+    for (size_t i = 0; i < ast->nb_children; i++)
+    {
+        if (ast->forest[i]->content.op_type == OPERATOR_FUNC_BODY)
+            return ast->forest[i];
+    }
+
+    return NULL;
+}
+
+int ast_handle_func_declaration(struct ast *ast,
+        struct execution_bundle *bundle)
+{
+    if (ast == NULL || ast->nb_children == 0)
+        return AST_ERROR;
+
+    struct ast *ast_func_name = find_op_function_name(ast);
+    struct ast *ast_func_body = find_op_function_body(ast);
+
+    if (ast_func_name == NULL || ast_func_body == NULL)
+        return AST_ERROR;
+
+    insert_func(bundle->hash_table_func, ast_func_name->content.value,
+            ast_func_body);
+
+    return AST_SUCCESS;
+}
