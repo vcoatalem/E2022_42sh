5a1132a5bf2468c487346a56fbe8d027ec07f1a3
alexandre <alexandre.lyfoung@epita.fr>
2019-11-21 19:57:57 +0100

    [BUILTIN][SHOPT] add Doxygen documentation + add builtin_shopt in builtins.h

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -6,5 +6,18 @@ typedef int(*builtin_handler)(char **str, size_t size, struct execution_bundle *
 builtin_handler str_to_builtin(char *name);
 int builtin_history(char **str, size_t size, struct execution_bundle *bundle);
 
+/**
+ * \brief Execute the builtin shopt
+ *
+ * Builtin shopt, display 42sh option , has optional parameter ("-s", "-u", "-q")
+ *
+ * \param **str array of string 
+ * \param size size of **str
+ * \param *bundle 
+ *
+ * \return 0 , 1 if failed
+ *
+ */
+int builtin_shopt(char **str, size_t size, struct execution_bundle *bundle);
 
 #endif/* ( ! BUILTINS_H) */
