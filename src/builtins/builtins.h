94fb6ce76c93f2b659fed84f326c11196fa709ae
Arthur Pannier <arthur.pannier@epita.fr>
2019-11-21 18:37:48 +0100

    [BUILTINS] add history builtin

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -1,9 +1,10 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
+#include "../main/42sh.h"
 
-typedef int(*builtin_handler)(char *str);
-
-
+typedef int(*builtin_handler)(char **str, size_t size, struct execution_bundle *bundle);
+builtin_handler str_to_builtin(char *name);
+int builtin_history(char **str, size_t size, struct execution_bundle *bundle);
 
 
 #endif/* ( ! BUILTINS_H) */
