cdf3b5c07791751cbd2331edc309134bf17b54d6
Arthur Pannier <arthur.pannier@epita.fr>
2019-11-22 16:38:24 +0100

    [BUILTIN][HISTORY] correction of coding style

--- src/builtins/history.c
+++ src/builtins/history.c
@@ -18,6 +18,30 @@ static int error_fd( char* file)
 }
 
 
+static int builtin_history2(char **str, size_t size, void *bundle_ptr)
+{
+    char *line = NULL;
+    size_t sizeline;
+    if (!bundle_ptr)
+        return 1;
+    struct execution_bundle *bundle = bundle_ptr;
+    char *historyfile = get_variable(bundle->hash_table_var, "HISTFILE");
+    if (size == 2 && strcmp(str[0], "-r"))
+    {
+        FILE* fdhistoy = fopen(historyfile, "a");
+        if (!fdhistoy)
+            return error_fd(".42sh_history");
+        FILE* fdfile = fopen(str[1], "r");
+        if (!fdfile)
+            return error_fd(str[1]);
+        while(getline(&line, &sizeline, fdfile) != -1)
+            fputs(line, fdhistoy);
+        fclose(fdhistoy);
+        fclose(fdfile);
+    }
+    return SUCCESS;
+}
+
 
 int builtin_history(char **str, size_t size, void *bundle_ptr)
 {
@@ -43,18 +67,5 @@ int builtin_history(char **str, size_t size, void *bundle_ptr)
             return error_fd(".42sh_history");
         fclose(fd);
     }
-    else if (size == 2 && strcmp(str[0], "-r"))
-    {
-        FILE* fdhistoy = fopen(historyfile, "a");
-        if (!fdhistoy)
-            return error_fd(".42sh_history");
-        FILE* fdfile = fopen(str[1], "r");
-        if (!fdfile)
-            return error_fd(str[1]);
-        while(getline(&line, &sizeline, fdfile) != -1)
-            fputs(line, fdhistoy);
-        fclose(fdhistoy);
-        fclose(fdfile);
-    }
-    return SUCCESS;
+    return builtin_history2(str, size, &bundle_ptr);
 }
