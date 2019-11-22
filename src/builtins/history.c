c8e74df5c0006ca114c89a3b569e301813da19f4
Arthur Pannier <arthur.pannier@epita.fr>
2019-11-22 15:36:32 +0100

    [BUILTIN] add the default location of home for history

--- src/builtins/history.c
+++ src/builtins/history.c
@@ -1,9 +1,9 @@
 #include "builtins.h"
-
-
+#include "../main/42sh.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <err.h>
 
 enum
 {
@@ -13,21 +13,23 @@ enum
 
 static int error_fd( char* file)
 {
-    printf("No %s file\n", file);
+    warn("No %s file\n", file);
     return ERROR_INVALID_FILE;
 }
 
 
 
-int builtin_history(char **str, size_t size, void *bundle)
+int builtin_history(char **str, size_t size, void *bundle_ptr)
 {
     char *line = NULL;
     size_t sizeline;
-    if (!bundle)
+    if (!bundle_ptr)
         return 1;
-    else if (!size)
+    struct execution_bundle *bundle = bundle_ptr;
+    char *historyfile = get_variable(bundle->hash_table_var, "HISTFILE");
+    if (!size)
     {
-        FILE *fd = fopen(".42sh_history", "r");
+        FILE *fd = fopen(historyfile, "r");
         if (!fd)
             return error_fd(".42sh_history");
         while(getline(&line, &sizeline, fd) != -1)
@@ -36,14 +38,14 @@ int builtin_history(char **str, size_t size, void *bundle)
     }
     else if (strcmp(str[0], "-c"))
     {
-        FILE *fd = fopen(".42sh_history", "w");
+        FILE *fd = fopen(historyfile, "w");
         if (!fd)
             return error_fd(".42sh_history");
         fclose(fd);
     }
     else if (size == 2 && strcmp(str[0], "-r"))
     {
-        FILE* fdhistoy = fopen(".42sh_history", "a");
+        FILE* fdhistoy = fopen(historyfile, "a");
         if (!fdhistoy)
             return error_fd(".42sh_history");
         FILE* fdfile = fopen(str[1], "r");
