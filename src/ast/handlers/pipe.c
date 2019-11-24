a1dd6b63ac48556892bbc3c98b421af9d1a13f7d
Victor Coatalem <victor.coatalem@epita.fr>
2019-11-24 07:01:20 +0100

    [AST][TESTS] fixed ast execution tests that broke with previous changes to pipeline execution

--- src/ast/handlers/pipe.c
+++ src/ast/handlers/pipe.c
@@ -15,9 +15,14 @@ int ast_handle_pipe(struct ast *ast, void *bundle_ptr)
     {
         struct ast *ast_cmd = find_op_type(ast, OPERATOR_COMMAND);
         struct command *cmd = get_command(ast_cmd, bundle_ptr);
-
+        printf("adding command to pipe\n");
         pipe_add_command(pipe, cmd);
         ast = find_op_type(ast, OPERATOR_PIPE);
+        if (ast)
+        {
+            //go on the pipe child of the pipeline node
+            ast = find_op_type(ast, OPERATOR_PIPE);
+        }
     }
 
     int result = pipe_execute(pipe, bundle_ptr);
