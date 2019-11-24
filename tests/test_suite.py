13144adcc14ff34e890406fd0b1e66ac4ab9ce44
alexandre <alexandre.lyfoung@epita.fr>
2019-11-24 02:13:56 +0100

    [TESTS][TEST_SUITE] add other test + add option -c + -l + -t

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -5,25 +5,35 @@ from termcolor import colored
 
 import subprocess as sp
 import yaml
+import time
  
 
 def run_42sh(args, stdin):
     return sp.run(args, capture_output=True, text=True, input=stdin)
 
 def test(binary, tests):
+    start_time = time.time()
     ref = run_42sh(["bash", "--posix"], tests["stdin"])
     sh = run_42sh([binary], tests["stdin"])
     for check in tests.get("checks", ["stdout", "stderr", "returncode"]):
         if check == "stdout":
+            print(tests["name"] + "execution time: %.10s" % (time.time() -
+                        start_time))
             assert ref.stdout == sh.stdout, \
                 f"stdout differs:\n{diff(ref.stdout, sh.stdout)}"
         elif check == "stderr":
+            print(tests["name"] + "execution time: %.10s" % (time.time() -
+                        start_time))
             assert ref.stderr == sh.stderr, \
                 f"stderr differs:\n{diff(ref.stderr, sh.stderr)}"
         elif check == "returncode":
+            print(tests["name"] + "execution time: %.10s" % (time.time() -
+                        start_time))
             assert ref.returncode == sh.returncode, \
                 f"Exit with {sh.returncode} , got {ref.returncode}"
         elif check == "has_stderr":
+            print(tests["name"] + "execution time: %.10s" % (time.time() -
+                        start_time))
             assert sh.stderr != "", "Something was expected on stderr"
 
 def diff(ref, sh):
@@ -33,21 +43,57 @@ def diff(ref, sh):
     return ''.join(unified_diff(ref, sh, fromfile="ref", tofile="42sh"))
 
 if __name__ == "__main__":
+    test_time = time.time()
     parser = ArgumentParser(description = "42sh TestSuite")
     parser.add_argument('bin', metavar='BIN')
-
+    parser.add_argument("-l", "--list", action="store True")
+    parser.add_argument("-c", "--category", type=str)
+    parser.add_argument("-t", "--timeout", type=int)
     args = parser.parse_args();
-
     binary = Path(args.bin).absolute()
-
-    with open("tests.yml", "r") as tests_files:
-        test_case = yaml.safe_load(tests_files)
-
-    for tests in test_case:
-        try:
-            test(binary, tests)
-        except AssertionError as err:
-            print(f"[{colored('KO', 'red')}]", tests["name"])
-            print(err)
+    if args.list:
+        sp.call(["ls", "list-test/"])
+    else:
+        success = 0
+        fail = 0
+        if args.category:
+            with open(args.category, "r") as tests_files:
+                test_case = yaml.safe_load(tests_files)
+            for tests in test_case:
+                try:
+                    test(binary, tests)
+                except AssertionError as err:
+                    print(f"[{colored('KO', 'red')}]", tests["name"])
+                    print(err)
+                    fail = fail + 1
+                else:
+                    print(f"[{colored('OK', 'green')}]", tests["name"])
+                    success = success + 1
         else:
-            print(f"[{colored('OK', 'green')}]", tests["name"])
+            path_list = []
+            for (directories, _, sub_files) in os.walk("list-test/"):
+                for files in sub_files:
+                    path_file = directories + '/' + files
+                    path_list.append(path_file)
+            for elmt in path_list:
+                with open("tests.yml", "r") as tests_files:
+                    test_case = yaml.safe_load(tests_files)
+            for tests in test_case:
+            try:
+                test(binary, tests)
+            except AssertionError as err:
+                print(f"[{colored('KO', 'red')}]", tests["name"])
+                print(err)
+                fail = fail + 1
+            except TimeoutError as err:
+                
+                print(err)
+            else:
+                print(f"[{colored('OK', 'green')}]", tests["name"])
+                success = success + 1
+        print("------------ Tests Results -------------")
+        print("Numbers of tests:",success + fail)
+        print(f"Tests succeed: {colored(sucess, 'green')}")
+        print(f"Tests failed: {colored(fail, 'red')}")
+        print("Time to execute all tests: %.s" % (time.time() - test_time))
+        print("----------------------------------------")
