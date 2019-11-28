d4482c31437c596b7a8db74b4873d4cea4a8d581
J <kyu.shulk@gmail.com>
2019-11-28 17:41:25 +0100

    [EXPANSION] Removing all functions as fnmatch does already wildcard check, add get_find function

--- src/expansion/expansion.h
+++ src/expansion/expansion.h
@@ -4,6 +4,7 @@
  * \author alexandre.lyfoung arthur.pannier julien.chau victor.coatalem
  * \version 1.0
  *
+ * Path expansion
  *
  */
 
@@ -12,16 +13,7 @@
 
 #include <stddef.h>
 
-#define EXPANSION_SUCCESS 0
-#define EXPANSION_FAILURE 1
-#define EXPANSION_ERROR 2
-
-void clean_pattern(char *pattern);
-
-int check_star(char *path, size_t *index, size_t size, char next);
-
-int check_question_mark(size_t *index, size_t size);
-
-int check_brackets(char *path, size_t *index, size_t size, char *brackets);
+void get_subdirectories(const char *pattern, char *path,
+                            char ***arguments, size_t *nb);
 
 #endif
