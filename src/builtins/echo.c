4e477548fdeae1d1345657dd3d4c6503381d7de3
J <kyu.shulk@gmail.com>
2019-12-06 10:13:13 +0100

    [BUILTINS] Add flags struct for refacto, add octal and hexa escaped character cases

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,8 +1,51 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "builtins.h"
 #include "../main/42sh.h"
+
+struct flags
+{
+    int trailing_newline_set;
+    int enable_backslash_set;
+    int disable_backslash_set;
+
+};
+
+static struct flags *flags_init(void)
+{
+    struct flags *f = calloc(1, sizeof(struct flags));
+    f->trailing_newline_set = 0;
+    f->enable_backslash_set = 0;
+    f->disable_backslash_set = 1;
+
+    return f;
+}
+
+static void flags_reset(struct flags *f)
+{
+    f->trailing_newline_set = 0;
+    f->enable_backslash_set = 0;
+    f->disable_backslash_set = 1;
+}
+
+static void set_newline(struct flags *f)
+{
+    f->trailing_newline_set = 1;
+}
+
+static void set_enable_backslash(struct flags *f)
+{
+    f->enable_backslash_set = 1;
+    f->disable_backslash_set = 0;
+}
+
+static void set_disable_backslash(struct flags *f)
+{
+    f->enable_backslash_set = 0;
+    f->disable_backslash_set = 1;
+}
 
 static char *escaped_sequence(char *s, size_t index)
 {
@@ -28,9 +71,11 @@ static char *escaped_sequence(char *s, size_t index)
         c[0] = '\v';
     else if (s[index] == '0')
     {
-    }
+        c[0] = strtol(s + index + 1, NULL, 8);
+   }
     else if (s[index] == 'x')
     {
+        c[0] = strtol(s + index + 1, NULL, 16);
     }
 
     return c;
@@ -54,15 +99,35 @@ static void _printf_escaped(char *s)
     }
 }
 
+static void _print(char **argv, size_t size, size_t index, struct flags *f)
+{
+    if (f->enable_backslash_set == 1)
+    {
+        for (; index < size - 1; index++)
+        {
+            _printf_escaped(argv[index]);
+            printf(" ");
+        }
+        _printf_escaped(argv[index]);
+    }
+ 
+    if (f->disable_backslash_set == 1)
+    {
+        for (; index < size - 1; index++)
+            printf("%s " , argv[index]);
+        printf("%s", argv[index]);
+    }
+ 
+    if (f->trailing_newline_set == 0)
+        printf("\n");
+}
+
 int builtin_echo(char **argv, size_t size, void *bundle_ptr)
 {
     if (bundle_ptr == NULL)
         return 1;
 
-    int trailing_newline_set =  0;
-    int enable_backslash_set = 0;
-    int disable_backslash_set = 1;
-
+    struct flags *f = flags_init();
     if (size >= 2)
     {
         size_t i = 0;
@@ -71,42 +136,24 @@ int builtin_echo(char **argv, size_t size, void *bundle_ptr)
             for (int j = 1; argv[i][j] != '\0'; j++)
             {
                 if (argv[i][j] == 'n')
-                    trailing_newline_set = 1;
+                    set_newline(f);
 
-                if (argv[i][j] == 'e')
-                {
-                    enable_backslash_set = 1;
-                    disable_backslash_set = 0;
-                }
+                else if (argv[i][j] == 'e')
+                    set_enable_backslash(f);
 
-                if (argv[i][j] == 'E')
+                else if (argv[i][j] == 'E')
+                    set_disable_backslash(f);
+
+                else
                 {
-                    disable_backslash_set = 1;
-                    enable_backslash_set = 0;
+                    flags_reset(f);
+                    printf("%s ", argv[i]);
+                    break;
                 }
             }
         }
 
-        if (enable_backslash_set == 1)
-        {
-            for (; i < size - 1; i++)
-            {
-                _printf_escaped(argv[i]);
-                printf(" ");
-            }
-            _printf_escaped(argv[i]);
-        }
-
-        if (disable_backslash_set == 1)
-        {
-            for (; i < size - 1; i++)
-                printf("%s " , argv[i]);
-            printf("%s", argv[i]);
-        }
-
-        if (trailing_newline_set == 0)
-            printf("\n");
-
+        _print(argv, size, i, f);
     }
 
     return 0;
